3550e93dd3cbc927fe115cb04280cc2f9d75b409
Ting <ticccccc@163.com>
2018-04-22 18:22:41 +0800

    update

--- app/src/main/java/com/work/ddnet/I.kt
+++ app/src/main/java/com/work/ddnet/I.kt
@@ -6,29 +6,6 @@ package com.work.ddnet
  */
 
 interface I {
-    interface Configkey {
-        companion object {
-            val CONFIG_READY = "CONFIG_READY"
-            val APPLICATION_CONTEXT = "APPLICATION_CONTEXT"
-            val API_HOST = "API_HOST"
-            val LOADER_DELAYED = "LOADER_DELAYED"
-            val HANDLER = "HANDLER"
-            val INTERCEPTOR = "INTERCEPTOR"
-        }
-    }
-
-    interface HttpMethod {
-        companion object {
-            val GET = "GET"
-            val POST = "POST"
-            val POST_RAW = "POST_RAW"
-            val PUT = "PUT"
-            val PUT_RAW = "PUT_RAW"
-            val DELETE = "DELETE"
-            val UPLOAD = "UPLOAD"
-        }
-    }
-
     interface BaseUrl {
         companion object {
             val URL = "http://127.0.0.1/"
